418ec061ea41534e23f38ff64ef02169b1c77162
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-03-01 22:48:01 -0500

    Update mod to Minecraft 1.18.2
    Fixes #6

--- src/main/java/xyz/nucleoid/anonymize/mixin/MixinPlayerManager.java
+++ src/main/java/xyz/nucleoid/anonymize/mixin/MixinPlayerManager.java
@@ -1,6 +1,6 @@
 package xyz.nucleoid.anonymize.mixin;
 
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -10,8 +10,9 @@ import net.minecraft.server.PlayerManager;
 @Mixin(PlayerManager.class)
 public class MixinPlayerManager {
 
-	@Redirect(method = "onPlayerConnect", at = @At(target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;)V", value = "INVOKE"))
-	private void hideIP(Logger logger, String message, Object name, Object ip, Object id, Object x, Object y, Object z) {
-		logger.info(message, name, "hidden ip", id, x, y, z);
+	@Redirect(method = "onPlayerConnect", at = @At(target = "Lorg/slf4j/Logger;info(Ljava/lang/String;[Ljava/lang/Object;)V", value = "INVOKE"))
+	private void hideIP(Logger logger, String message, Object[] arguments) {
+		arguments[1] = "hidden ip";
+		logger.info(message, arguments);
 	}
 }
